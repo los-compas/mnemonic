44ebd6d0c0481a22efd8fcaf9e2192ea3ad47cd5
caldrete <cesaraaldrete@gmail.com>
2022-01-27 16:11:22 -0800

    duplicate files removed

--- Context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-export const contactformContext = createContext(null);
-
-export const invoiceContext = createContext(null);
-
-export const regContext = createContext(null);
